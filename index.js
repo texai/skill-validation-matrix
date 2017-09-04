2c729969b072a9dcb169596c44738e2e31ede28f
Ernesto Anaya <ernestex@gmail.com>
2017-09-03 23:05:28 -0500

    WIP JS

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ $(function(){
 	//$('.bar').hide();
 
 	$('table.selector tbody td').on('mouseover',function(){
-		$('.bar.'+$(this).data('class')).show();
+		//$('.bar.'+$(this).data('class')).show();
+		$('.bar').css('visibility','hidden');
+		$('.bar.'+$(this).data('class')).css('visibility','visible');
 	});
 	$('table.selector tbody td').on('mouseleave',function(){
-		$('.bar').hide();
+		$('.bar').css('visibility','hidden');
+	});
+
+	$('table.selector tbody th').on('mouseover',function(){
+		$('.bar').css('visibility','visible');
+		//$('.bar').css('opacity','1');
+	});
+	$('table.selector tbody th').on('mouseleave',function(){
+		//$('.bar').css('visibility','hidden');
+		//$('.bar').css('opacity','0');
 	});
 
 
